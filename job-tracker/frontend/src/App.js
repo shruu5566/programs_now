7a27bbeae6aa0079f6459f538308c123e9d9845b
SHRUSHTI <shrushtibp@gmail.com>
2026-01-26 22:18:23 +0530

    small changes

--- job-tracker/frontend/src/App.js
+++ job-tracker/frontend/src/App.js
@@ -1,7 +1,25 @@
+import { useState } from "react";
+
 function App() {
+  const [company, setCompany] = useState("");
+  const [role, setRole] = useState("");
+
   return (
     <div>
       <h1>Job Application Tracker</h1>
+      <input
+        placeholder="Company Name"
+        value={company}
+        onChange={(e) => setCompany(e.target.value)}
+      />
+      <br /><br />
+      <input
+        placeholder="Job Role"
+        value={role}
+        onChange={(e) => setRole(e.target.value)}
+      />
+      <br /><br />
+      <button>Add Job</button>
     </div>
   );
 }
