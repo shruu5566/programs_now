d863bed80e5fccc4456647b589aa8ac7557d9b78
SHRUSHTI <shrushtibp@gmail.com>
2026-01-26 22:21:38 +0530

    added first job

--- job-tracker/frontend/src/App.js
+++ job-tracker/frontend/src/App.js
@@ -4,22 +4,41 @@ function App() {
   const [company, setCompany] = useState("");
   const [role, setRole] = useState("");
 
+  const addJob = async () => {
+    await fetch("http://localhost:5000/api/jobs", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ company, role })
+    });
+
+    alert("Job Added!");
+    setCompany("");
+    setRole("");
+  };
+
   return (
     <div>
       <h1>Job Application Tracker</h1>
+
       <input
         placeholder="Company Name"
         value={company}
         onChange={(e) => setCompany(e.target.value)}
       />
+
       <br /><br />
+
       <input
         placeholder="Job Role"
         value={role}
         onChange={(e) => setRole(e.target.value)}
       />
+
       <br /><br />
-      <button>Add Job</button>
+
+      <button onClick={addJob}>Add Job</button>
     </div>
   );
 }
