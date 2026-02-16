d5f440d6482facc02364d1811b0bbb4dd74fab64
SHRUSHTI <shrushtibp@gmail.com>
2026-02-16 23:09:46 +0530

    bakend changes

--- job-tracker/backend/server.js
+++ job-tracker/backend/server.js
@@ -30,7 +30,7 @@ mongoose
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.error("MongoDB Connection Error:", err));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
